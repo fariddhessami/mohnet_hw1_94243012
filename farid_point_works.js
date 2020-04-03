7b69e4ff7099266a4a401000cd66a48ed94d5958
fariddhessami <fariddhessami@gmail.com>
2020-04-03 20:20:32 +0430

    server detected the point lat : 79.453125 long : 63.54855223203644

--- farid_point_works.js
+++ farid_point_works.js
@@ -1,21 +1,30 @@
 const turf = require('@turf/turf');
 
-var pt = turf.point([-77, 44]);
-var poly = turf.polygon([
+module.exports = find_answer_polygons_names_for_point;
+
+function find_answer_polygons_names_for_point(lat, long, names_coords_array) {
+    var answer_names = [];
+
+    names_coords_array.forEach(element => {
+        console.log(`element.coords is : ${element.coords}`);
+
+        var turPoly = turf.polygon(element.coords);
+
+        if (turf.booleanPointInPolygon([lat, long], turPoly)) console.log('jesus');
+    });
+}
+
+var pt = turf.point([79.453125, 63.54855223203644]);
+var poly2 = turf.polygon([
     [
-        [-81, 41],
-        [-81, 47],
-        [-72, 47],
-        [-72, 41],
-        [-81, 41]
+        [56.25, 52.26815737376817],
+        [110.390625, 52.26815737376817],
+        [110.390625, 70.37785394109224],
+        [56.25, 70.37785394109224],
+        [56.25, 52.26815737376817]
     ]
 ]);
 
-turf.booleanPointInPolygon(pt, poly);
+console.log('the crusial answer is : ' + turf.booleanPointInPolygon(pt, poly2));
+
 //= true
-
-console.log(turf.booleanPointInPolygon(pt, poly));
-
-function find_answer_polygons_names_for_point(lat, long, names_coords_array) {
-    var suspect_name;
-}
--- index.js
+++ index.js
@@ -8,8 +8,9 @@ var say_sth_hi = module.require('./test_module_1');
 say_sth_hi();
 
 var reader = module.require('./farid_reader');
-var array_major = reader();
-console.log('ull see this after i read');
+var data_array = reader();
+
+var pointWorks = module.require('./farid_point_works');
 
 app.get('/', (req, res) => {
     res.send('hello world');
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     res.send('new post req recieved !!');
+});
+
+app.get('/gis/print_data/', (req, res) => {
+    res.send(data_array);
 });
 
 app.get('/gis/testpoint/', (req, res) => {
@@ -28,8 +33,13 @@ app.get('/gis/testpoint/:num', (req, res) => {
 });
 
 app.get('/gis/testpoint/:lat/:long', function(req, res) {
-    res.send('lat nad long' + req.params.lat + ' ' + req.params.long);
-    res.send(req.params);
+    let lat = req.params.lat;
+    let long = req.params.long;
+
+    res.send('lat nad long' + lat + ' ' + long);
+    console.log(`server recieved a point lat : ${lat} long : ${long}`);
+
+    pointWorks(lat, long, data_array);
 });
 
 app.listen(PORT, () => console.log(`port is listening right now : ${PORT}`));
