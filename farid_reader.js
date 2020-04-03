35a3b6d93a66d97674696adde19030474bc5a8fa
fariddhessami <fariddhessami@gmail.com>
2020-04-03 16:41:37 +0430

    a file reader added which takes data and puts it into an arry of format [name,coords]

---
+++ farid_reader.js
@@ -0,0 +1,21 @@
+let data_from_file = require('./data.json');
+//data_from_file is an object
+
+let features_1st_layer = data_from_file.features;
+
+console.log(features_1st_layer);
+
+var array_of_names_coords = [];
+
+features_1st_layer.forEach(feature => {
+    console.log('name : ' + feature.properties.name);
+    console.log('coordinates : ' + feature.geometry.coordinates);
+
+    let name = feature.properties.name;
+    let coords = feature.geometry.coordinates;
+
+    array_of_names_coords.push(name, coords);
+});
+
+console.log('real thing : ');
+console.log(array_of_names_coords);
