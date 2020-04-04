2c5140d98da4752c21fcf3852f6401268d9ceb88
fariddhessami <fariddhessami@gmail.com>
2020-04-04 15:30:25 +0430

    const PORT = process.env.PORT || 3000; has added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 //question : what does this line do ?
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 var say_sth_hi = module.require('./test_module_1');
 say_sth_hi();
